a60a4b2d14cb467409f883eebc390ec306eedafc
GradyThompson <grady_thompson@outlook.com>
2025-07-07 12:25:28 -0700

    restructured controller to allow for easier integration with RL environment wrapper

--- tradeoff/tradeoff/controller.py
+++ tradeoff/tradeoff/controller.py
@@ -2,9 +2,9 @@ from simulated_system import SimulatedSystem
 from job import Job
 from model import Model
 import job_manager
-import heapq
 from action import Action
 import result_manager
+import typing
 
 """
 Controls a state-based simulated system of jobs and machines
@@ -38,8 +38,62 @@ class Controller:
         self.model:Model = Model(module_name=model_module_name, class_name=model_class_name, params=model_params)
         self.results_file_name:str = results_file_name
         self.time:int = 0
+        self.wait_time:int = -1
 
         self.jobs:list[Job] = sorted(job_manager.jobs_from_file(input_jobs_file_name))
+        self.job_ind:int = 0
+        self.queued_jobs: list[Job] = []
+
+    """
+    Moves the controller to the next step, continues until the earlier of the wait time and the next job release time
+    
+    Returns:
+        The simulated system and queued jobs
+    """
+    def next_step(self)->typing.Tuple[SimulatedSystem, list[Job]]:
+        if self.wait_time == -1:
+            next_time = self.jobs[self.job_ind].get_receival_time()
+        elif self.job_ind == len(self.jobs):
+            next_time = self.wait_time
+        else:
+            next_time = min(self.jobs[self.job_ind].get_receival_time(), self.wait_time)
+        self.wait_time = -1
+        while self.job_ind < len(self.jobs) and self.jobs[self.job_ind].get_receival_time() <= next_time:
+            self.queued_jobs.append(self.jobs[self.job_ind])
+            self.job_ind += 1
+        self.system.run(next_time)
+
+        # Get and handle actions
+        actions: list[Action] = self.model.determine_actions(system=self.system, unassigned_jobs=self.queued_jobs)
+        for action in actions:
+            if action.get_action_type() == Action.WAIT:
+                self.wait_time = action.get_time()
+        self.system.perform_actions(actions=actions)
+        self.time = next_time
+
+        # Remove assigned jobs from queued
+        jobs_to_remove: list[Job] = []
+        assigned_jobs: set[Job] = self.system.get_assigned_jobs()
+        for job in self.queued_jobs:
+            if job in assigned_jobs:
+                jobs_to_remove.append(job)
+        for job in jobs_to_remove:
+            self.queued_jobs.remove(job)
+        return self.system, self.queued_jobs
+
+    """
+    Resets the system
+    
+    Returns:
+        The simulated system and queued jobs after reset
+    """
+    def reset(self)->typing.Tuple[SimulatedSystem, list[Job]]:
+        self.system.reset()
+        self.queued_jobs = []
+        self.job_ind = 0
+        self.time:int = 0
+        self.wait_time:int = -1
+        return self.system, self.queued_jobs
 
     """
     Runs the system until completion
@@ -48,38 +102,8 @@ class Controller:
         The name of the file with the results
     """
     def control_loop(self)->str:
-        wait_time:int = -1
-        job_ind:int = 0
-        queued_jobs: list[Job] = []
-        while job_ind < len(self.jobs) or wait_time != -1:
-            if wait_time == -1:
-                next_time = self.jobs[job_ind].get_receival_time()
-            elif job_ind == len(self.jobs):
-                next_time = wait_time
-            else:
-                next_time = min(self.jobs[job_ind].get_receival_time(), wait_time)
-            wait_time = -1
-            while job_ind < len(self.jobs) and self.jobs[job_ind].get_receival_time() <= next_time:
-                queued_jobs.append(self.jobs[job_ind])
-                job_ind += 1
-            self.system.run(next_time)
-
-            #Get and handle actions
-            actions:list[Action] = self.model.determine_actions(system=self.system, unassigned_jobs=queued_jobs)
-            for action in actions:
-                if action.get_action_type() == Action.WAIT:
-                    wait_time = action.get_time()
-            self.system.perform_actions(actions=actions)
-            self.time = next_time
-
-            #Remove assigned jobs from queued
-            jobs_to_remove:list[Job] = []
-            assigned_jobs:set[Job] = self.system.get_assigned_jobs()
-            for job in queued_jobs:
-                if job in assigned_jobs:
-                    jobs_to_remove.append(job)
-            for job in jobs_to_remove:
-                queued_jobs.remove(job)
+        while self.job_ind < len(self.jobs) or self.wait_time != -1:
+            self.next_step()
         time_until_done:int = self.system.get_time_until_done()
         self.system.run(self.time + time_until_done)
         result_manager.save_system_performance(self.system, self.jobs, self.results_file_name)
--- tradeoff/tradeoff/env_RL_wrapper.py
+++ tradeoff/tradeoff/env_RL_wrapper.py
@@ -2,24 +2,92 @@ import gymnasium
 from gymnasium import spaces
 from gymnasium.core import Env
 import numpy as np
-from typing import Tuple, Dict, Any
+import typing
+from controller import Controller
+from simulated_system import SimulatedSystem
+from job import Job
 
 class MyCustomEnv(Env):
     state: np.ndarray
+    steps: int
+    controller: Controller
 
-    def __init__(self):
-        pass #TODO
+    """
+    Constructor for the RL model
+    
+    Args:
+        config_file_name: name of the file containing the system config information
+    """
+    def __init__(self, config_file_name: str):
+        super().__init__()
+        self.steps = 0
+        self.controller = Controller(config_file_name=config_file_name)
 
-    def reset(self, *, seed: int | None = None, options: dict | None = None) -> Tuple[np.ndarray, Dict[str, Any]]:
-        return self.state, {} #TODO
+    """
+    Generates the state for the RL model given the system and queued jobs
+    """
 
-    def step(self, action: int) -> Tuple[np.ndarray, float, bool, Dict[str, Any]]:
+    def generate_state(self, system:SimulatedSystem, jobs:list[Job])->np.ndarray:
+        state: np.ndarray = np.ndarray([])
+        return state
+
+    """
+    Resets the RL system
+    
+    Args:
+        seed: the seed to generate the new system
+        options: additional options for the new system
+        
+    Returns:
+        Initial state of the system
+        Additional information associated with the initial system
+    """
+    def reset(self, *, seed: int | None = None, options: dict | None = None) -> typing.Tuple[np.ndarray, typing.Dict[
+        str, typing.Any]]:
+        system: SimulatedSystem
+        jobs: list[Job]
+
+        super().reset(seed=seed)
+        system, jobs = self.controller.reset()
+        self.steps = 0
+        self.state = self.generate_state(system, jobs)
+        return self.state, {}
+
+    """
+    Generates the reward of the action given the initial state and the next state
+    """
+    def get_reward(self, prev_system:SimulatedSystem, new_system:SimulatedSystem):
+        pass
+
+    """
+    Goes to the next step of the simulation
+    
+    Args:
+        action: the next action to be performed on the system
+        
+    Returns:
+        The state of the system after the action
+        The reward received after the action
+        Bool tracking if the system is done
+        Other information associated with the system
+    """
+    def step(self, action: int) -> typing.Tuple[np.ndarray, float, bool, typing.Dict[str, typing.Any]]:
         reward = 0
         done = True
+        self.steps += 1
         return self.state, reward, done, {} #TODO
 
+    """
+    Creates a human readable version of the system
+    
+    Args:
+        mode: the mode of display (only option is human currently)
+    """
     def render(self, mode: str = 'human'):
         pass #TODO
 
+    """
+    Closes the system
+    """
     def close(self):
         pass #TODO
